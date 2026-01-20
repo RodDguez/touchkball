9fa7534c705136caa7130db35664f94abf5e72b9
RodDguez <rodrigo.domgar@educa.jcyl.es>
2026-01-20 11:00:13 +0100

    Add files via upload

---
+++ javascript Rodrigo/javascriptmundiales_Rodrigo Domínguez García.js
@@ -0,0 +1,33 @@
+function M1971(){
+    document.getElementById("1971").style.visibility = "visible";
+}
+function M1980(){
+    document.getElementById("1980").style.visibility = "visible";
+}
+function M1982(){
+    document.getElementById("1982").style.visibility = "visible";
+}
+function M1984(){
+    document.getElementById("1984").style.visibility = "visible";
+}
+function M1987(){
+    document.getElementById("1987").style.visibility = "visible";
+}
+function M2000(){
+    document.getElementById("2000").style.visibility = "visible";
+}
+function M2004(){
+    document.getElementById("2004").style.visibility = "visible";
+}
+function M2011(){
+    document.getElementById("2011").style.visibility = "visible";
+}
+function M2015(){
+    document.getElementById("2015").style.visibility = "visible";
+}
+function M2019(){
+    document.getElementById("2019").style.visibility = "visible";
+}
+function M2023(){
+    document.getElementById("2023").style.visibility = "visible";
+}
